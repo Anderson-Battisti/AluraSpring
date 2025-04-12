d81bbdd4073e0589ac2158bf1e4a91d90de61d6b
Anderson Battisti <anderson.battisti@universo.univates.br>
2025-04-12 18:01:29 -0300

    Java spring, lambda and stream course final project: requirement of first and second inputs build, a significant part of the menu was done

--- src/main/java/battisti/anderson/alura_spring_lambdas_streams/final_challenge/JsonMappings/Brand.java
+++ src/main/java/battisti/anderson/alura_spring_lambdas_streams/final_challenge/JsonMappings/Brand.java
@@ -1,7 +1,27 @@
 package battisti.anderson.alura_spring_lambdas_streams.final_challenge.JsonMappings;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Brand
 {
+    @JsonProperty( "codigo" )
     private String code;
+    @JsonProperty( "nome" )
     private String name;
+
+    public String getCode()
+    {
+        return code;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "[ Código: " + this.code + ", Nome: " + this.name + " ]";
+    }
 }
--- src/main/java/battisti/anderson/alura_spring_lambdas_streams/final_challenge/controller/MenuController.java
+++ src/main/java/battisti/anderson/alura_spring_lambdas_streams/final_challenge/controller/MenuController.java
@@ -1,6 +1,8 @@
 package battisti.anderson.alura_spring_lambdas_streams.final_challenge.controller;
 
 import battisti.anderson.alura_spring_lambdas_streams.final_challenge.JsonMappings.Brand;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.net.http.HttpResponse;
@@ -20,16 +22,24 @@ public class MenuController
 
     public void runMenu()
     {
-        String  veihcleType = requireFirstInput();
+        String vehicleType = requireVehicleType();
 
-        String responseJson = FipeApiController.getInstance().makeRequest( veihcleType, FipeApiController.QueryType.VEHICLE_TYPE ).body();
+        String responseJson = FipeApiController.getInstance().makeRequest( vehicleType, FipeApiController.QueryType.VEHICLE_TYPE ).body();
 
-        ObjectMapper mapper = new ObjectMapper();
-        //todo
-        //List<Brand> brands = mapper.readValue( json,  );
+        List<Brand> brands = getListOfBrandsByJson( responseJson );
+
+        if ( brands == null )
+        {
+            return;
+        }
+
+        printBrands( brands );
+
+        String brandCode = requireBrandCode( brands );
+        //todo the rest
     }
 
-    public String requireFirstInput()
+    public String requireVehicleType()
     {
         Scanner reader        = new Scanner( System.in );
         String  vehicleChoice = null;
@@ -48,5 +58,50 @@ public class MenuController
         }
 
         return vehicleChoice;
+    }
+
+    public String requireBrandCode( List<Brand> brands )
+    {
+        Scanner reader = new Scanner( System.in );
+
+        final String[] code   = new String[1];
+
+        System.out.println( "Escolha o código da marca que deseja visualizar os veículos: " );
+        code[0] = reader.nextLine();
+
+        while ( ! brands.stream().anyMatch( b -> b.getCode().equals( code[0] ) ) )
+        {
+            System.out.println( "Código inválido, digite novamente: " );
+            code[0] = reader.nextLine();
+        }
+
+        return code[0];
+    }
+
+    private List<Brand> getListOfBrandsByJson( String json )
+    {
+        try
+        {
+            ObjectMapper mapper = new ObjectMapper();
+
+            List<Brand> brands = mapper.readValue( json, new TypeReference<>() {} );
+
+            return brands;
+        }
+
+        catch ( JsonProcessingException e )
+        {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public void printBrands( List<Brand> brands )
+    {
+        for ( Brand brand : brands )
+        {
+            System.out.println( brand );
+        }
     }
 }
