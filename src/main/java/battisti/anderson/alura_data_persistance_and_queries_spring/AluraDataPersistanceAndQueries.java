e43624f69206980a40fc6880e17380b8e48599ba
Anderson Battisti <anderson.battisti@universo.univates.br>
2025-04-15 22:23:33 -0300

    Java: data persistance and queries with spring JPA: Package structure and main class for the course created

---
+++ src/main/java/battisti/anderson/alura_data_persistance_and_queries_spring/AluraDataPersistanceAndQueries.java
@@ -0,0 +1,20 @@
+package battisti.anderson.alura_data_persistance_and_queries_spring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+public class AluraDataPersistanceAndQueries
+    implements
+        CommandLineRunner
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run( AluraDataPersistanceAndQueries.class, args );
+    }
+
+    @Override
+    public void run( String... args ) throws Exception
+    {
+
+    }
+}
